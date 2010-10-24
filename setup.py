26087e78a4f50d9d843f9878a4d9b145e17f927f
Simon Käser <skaeser@raptus.com>
2010-10-24 12:58:38 +0000

    set zip_safe flag to False and prepared for release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0b1'
+version = '1.0b2'
 
 setup(name='Products.VisitTracker',
       version=version,
@@ -21,7 +21,7 @@ setup(name='Products.VisitTracker',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
       include_package_data=True,
-      zip_safe=True,
+      zip_safe=False,
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
